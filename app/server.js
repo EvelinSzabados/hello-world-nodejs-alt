7542e333691bb3b67d842040851ec8a8e22ae454
EvelinSzabados <45736392+EvelinSzabados@users.noreply.github.com>
2023-03-23 11:32:31 +0100

    test: check environment variable syntax

--- app/server.js
+++ app/server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', async (req, res) => {
+    console.log(process.env.CUSTOM_ENV)
+    console.log(process.env.SPECIAL_ENV)
+    console.log(process.env.ANOTHER_SPECIAL)
+    console.log(process.env.NUMBER_ENV)
     res.send("Hello World")
 })
 
