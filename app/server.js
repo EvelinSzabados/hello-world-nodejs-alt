ced5ab3066f41a810f893411540ce118dd31d0b4
EvelinSzabados <45736392+EvelinSzabados@users.noreply.github.com>
2023-03-23 14:39:40 +0100

    test: log isNumber

--- app/server.js
+++ app/server.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const isNumber = /^-?\d+$/.test(process.env.NUMBER_ENV)
 console.log(process.env.CUSTOM_ENV)
 console.log(process.env.CUSTOM_ENV.replace(/['‘’"“”]/g, ''))
 console.log(process.env.SPECIAL_ENV)
 console.log(process.env.ANOTHER_SPECIAL)
 console.log(process.env.NUMBER_ENV)
-console.log(typeof process.env.NUMBER_ENV)
+console.log(isNumber)
 
 app.get('/', async (req, res) => {
     res.send("Hello World")
